309a8173aae71830627648358e372df53e592e5b
Niek Stortenbeker <niekns@gmail.com>
2020-04-27 13:21:51 +0200

    started using pre-commit hooks with flake8, black and isort

--- kulturweb/__init__.py
+++ kulturweb/__init__.py
@@ -12,22 +12,22 @@ def main(global_config, **settings):
 
 
 def init_includes(config):
-    config.include('pyramid_chameleon')
+    config.include("pyramid_chameleon")
 
 
 def init_routing(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view("static", "static", cache_max_age=3600)
 
     # home controller
-    config.add_route('home', '/')
-    config.add_route('about', '/about')
+    config.add_route("home", "/")
+    config.add_route("about", "/about")
 
     # filter options
     # TODO also add only /kino etc.
     # http://localhost:6543/kino/wann=heute+synchronisierter=ja
-    config.add_route('filter', '/{what}/wann={when}+synchronisierter={dubbed}')
-    config.add_route('filter/', '/{what}/wann={when}+synchronisierter={dubbed}/')
+    config.add_route("filter", "/{what}/wann={when}+synchronisierter={dubbed}")
+    config.add_route("filter/", "/{what}/wann={when}+synchronisierter={dubbed}/")
 
-    config.add_route('package_details', '/project/test={package_name}')
+    config.add_route("package_details", "/project/test={package_name}")
     #               http://localhost:6543/project/awscli/releases/1.15.30
     config.scan()
--- setup.py
+++ setup.py
@@ -1,52 +1,46 @@
 import os
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, "README.txt")) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
+with open(os.path.join(here, "CHANGES.txt")) as f:
     CHANGES = f.read()
 
 requires = [
-    'plaster_pastedeploy',
-    'pyramid',
-    'pyramid_chameleon',
-    'pyramid_debugtoolbar',
-    'waitress',
+    "plaster_pastedeploy",
+    "pyramid",
+    "pyramid_chameleon",
+    "pyramid_debugtoolbar",
+    "waitress",
 ]
 
 tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest >= 3.7.4',
-    'pytest-cov',
+    "WebTest >= 1.3.1",  # py3 compat
+    "pytest >= 3.7.4",
+    "pytest-cov",
 ]
 
 setup(
-    name='kulturweb',
-    version='0.0',
-    description='kulturweb',
-    long_description=README + '\n\n' + CHANGES,
+    name="kulturweb",
+    version="0.0",
+    description="kulturweb",
+    long_description=README + "\n\n" + CHANGES,
     classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
     ],
-    author='Niek Stortenbeker',
-    author_email='niek@kulturbremen.de',
-    url='https://github.com/niekstortenbeker/kulturweb',
-    keywords='web pyramid pylons',
+    author="Niek Stortenbeker",
+    author_email="niek@kulturbremen.de",
+    url="https://github.com/niekstortenbeker/kulturweb",
+    keywords="web pyramid pylons",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
+    extras_require={"testing": tests_require,},
     install_requires=requires,
-    entry_points={
-        'paste.app_factory': [
-            'main = kulturweb:main',
-        ],
-    },
+    entry_points={"paste.app_factory": ["main = kulturweb:main",],},
 )
