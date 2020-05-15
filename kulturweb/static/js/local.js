2070b35dca4dfbf14b91562db138149c1dc8e3a5
Niek Stortenbeker <niekns@gmail.com>
2020-05-15 14:55:13 +0200

    major redesign

--- kulturweb/static/js/local.js
+++ kulturweb/static/js/local.js
@@ -2,34 +2,43 @@ $(document).ready(function(){
 	$('#info').tooltip({ trigger: 'click hover focus' });
 });
 
-var $toggleButtons = $('.toggle-content');
-var $fullTextWrappers = $('.fulltext');
-var fullText;
-var toggleButtonText;
+// description show more show less
+
+let $toggleButtons = $('.toggle-content');
+let $toggleButtonsDescription = $('.toggle-content-description')
+let $fullTextWrappers = $('.fulltext');
+let fullText;
+let toggleButtonText;
 
 $fullTextWrappers.attr('hidden', true);
 
 $toggleButtons.removeAttr('hidden');
+$toggleButtonsDescription.removeAttr('hidden');
+
+function showMorePattern(beforeText) {
+    return function () {
+        $(this).on('click', function () {
+
+            fullTextWrapper = $(this).parent().find('.fulltext');
+            toggleButtonText = $(this).find('.text');
+
+            console.log(fullTextWrapper);
+
+            // change attributes and text if full text is shown/hidden
+            if (!fullTextWrapper.attr('hidden')) {
+                toggleButtonText.text(beforeText);
+                fullTextWrapper.attr('hidden', true);
+                $(this).attr('aria-expanded', false);
+            } else {
+                toggleButtonText.text('weniger anzeigen');
+                fullTextWrapper.removeAttr('hidden');
+                $(this).attr('aria-expanded', true);
+            }
+        });
+    };
+
+};
 
 // add listener for each button
-$toggleButtons.each(function () {
-
-    $(this).on('click', function () {
-
-        fullTextWrapper = $(this).parent().find('.fulltext');
-        toggleButtonText = $(this).find('.text');
-
-        console.log(fullTextWrapper);
-
-        // change attributes and text if full text is shown/hidden
-        if (!fullTextWrapper.attr('hidden')) {
-            toggleButtonText.text('mehr anzeigen');
-            fullTextWrapper.attr('hidden', true);
-            $(this).attr('aria-expanded', false);
-        } else {
-            toggleButtonText.text('weniger anzeigen');
-            fullTextWrapper.removeAttr('hidden');
-            $(this).attr('aria-expanded', true);
-        }
-    });
-});
+$toggleButtons.each(showMorePattern('mehr anzeigen'));
+$toggleButtonsDescription.each(showMorePattern('Beschreibung'));
