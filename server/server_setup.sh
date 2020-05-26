86b21e1ed7968e4a9155f562a03a705befc1b3f3
Niek Stortenbeker <niekns@gmail.com>
2020-05-26 17:44:43 +0200

    extra notes

--- server/server_setup.sh
+++ server/server_setup.sh
@@ -8,20 +8,24 @@ apt upgrade -y
 # Install some OS dependencies:
 sudo apt-get install -y -q build-essential git 
 sudo apt-get install -y -q python3-pip python3-dev python3-venv
-sudo apt-get install -y -q unzip wget git
+sudo apt-get install -y -q unzip wget
 sudo apt-get install -y -q nginx
 # for gzip support in uwsgi
 sudo apt-get install --no-install-recommends -y -q libpcre3-dev libz-dev nload
+# tree
+apt install tree
 
 # Stop the hackers
+# block ssh users that try repeatedly to login
 sudo apt install fail2ban -y
 
+# only allow ssh, http and https. setup firewall for everything else.
 ufw allow 22
 ufw allow 80
 ufw allow 443
 ufw enable
 
-# Basic git setup
+# Basic git setup to cache password
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=720000'
 
@@ -35,23 +39,27 @@ chmod 777 /apps
 mkdir /apps/logs
 mkdir /apps/logs/kulturweb
 mkdir /apps/logs/kulturweb/app_log
-cd /apps
+cd /apps || exit
 
 # Create a virtual env for the app.
-cd /apps
+cd /apps || exit
 python3 -m venv venv
 source /apps/venv/bin/activate
 pip install --upgrade pip setuptools
+# httpie: like curl. glances: look at processes
 pip install --upgrade httpie glances
 pip install --upgrade uwsgi
 
 
 # clone the repo:
-cd /apps
-git clone https://github.com/talkpython/data-driven-web-apps-with-pyramid-and-sqlalchemy app_repo
+cd /apps || exit
+git clone https://github.com/niekstortenbeker/kultur app_repo/kultur
+git clone https://github.com/niekstortenbeker/kulturweb app_repo/kulturweb
 
 # Setup the web app:
-cd /apps/app_repo/kulturweb
+cd /apps/app_repo/kultur || exit
+python setup.py develop
+cd /apps/app_repo/kulturweb || exit
 # pip install -r requirements.txt
 python setup.py develop
 
