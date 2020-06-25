8d266c1f7a9206b4130d76609fc518850597cb5f
Niek Stortenbeker <niekns@gmail.com>
2020-06-25 13:40:06 +0200

    start using real database

--- kulturweb/__init__.py
+++ kulturweb/__init__.py
@@ -1,4 +1,4 @@
-from kultur import init_fake_database
+from kultur import init_database
 from pyramid.config import Configurator
 
 __version__ = "0.1"
@@ -11,6 +11,6 @@ def main(global_config, **settings):
     with Configurator(settings=settings) as config:
         config.include("pyramid_chameleon")
         config.include(".routes")
-        init_fake_database()
+        init_database()
         config.scan()
     return config.make_wsgi_app()
